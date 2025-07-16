abfb64839d3990102eadd42b6e48eda2b6a88f71
Laarni Buenaventura <youremail@example.com>
2025-07-15 20:50:28 -0500

    Updated Login/Register page: replaced st.experimental_rerun with st.rerun, moved 'New user?' message above email field

--- pages/1_Login_Register.py
+++ pages/1_Login_Register.py
@@ -19,9 +19,9 @@ tab_login, tab_register = st.tabs(["Login", "Register"])
 
 # -------------------- Login Tab --------------------
 with tab_login:
-    st.header("🔐 Login to Your Account")
+    st.header("Login to Your Account")
 
-    st.info("New here? Please register an account using the **Register** tab.")
+    st.info("New here? Please create an account using the **Register** tab.")
 
     login_email = st.text_input("Email", key="login_email")
     login_password = st.text_input("Password", type="password", key="login_password")
@@ -38,7 +38,7 @@ with tab_login:
 
 # -------------------- Register Tab --------------------
 with tab_register:
-    st.header("📝 Register a New Account")
+    st.header("Register a New Account")
 
     name = st.text_input("Full Name", key="register_name")
     register_email = st.text_input("Email", key="register_email")
